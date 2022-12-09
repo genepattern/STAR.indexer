da4fec35928ed19ae7862d5386ee2e284366fbc1
ted Liefeld <liefeld@broadinstitute.org>
2022-12-09 07:07:38 -0800

    Update to 2.7.1a

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # copyright 2017-2018 Regents of the University of California and the Broad Institute. All rights reserved.
-FROM  genepattern/docker-staraligner:v0.43
+FROM  genepattern/docker-staraligner:v0.45
 
-COPY star_indexer_wrapper.pl /usr/local/bin/star_aligner_wrapper.pl
+COPY star_indexer_wrapper.pl /usr/local/bin/star_indexer_wrapper.pl
 COPY ./* /build/
 
 
---
+++ tests/runConfig.sh
@@ -0,0 +1,16 @@
+
+INPUT_FILE_DIRECTORIES=$PWD/data
+TASKLIB=$PWD/src/
+
+COMMAND_LINE="perl /usr/local/bin/star_indexer_wrapper.pl --fastafilelist $INPUT_FILE_DIRECTORIES/reads.list.txt  --indexdir Genome --overhang 100 --indexstringlength 13 --binsize 18 --RAM 31"
+
+CONTAINER_OVERRIDE_MEMORY=3100
+JOB_DEFINITION_NAME="STAR"
+JOB_ID=STAR_ALGN_$1
+JOB_QUEUE=TedTest
+S3_ROOT=s3://moduleiotest
+WORKING_DIR=$PWD/job_52345
+
+DOCKER_CONTAINER=genepattern/star.indexer:v0.45
+
+
